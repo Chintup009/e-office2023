f5ed377c3b723ac9def12fb6d52fbf8f7601a7da
pttongnaka@gmail.com <lagacy.tm@gmail.com>
2023-03-27 10:35:17 +0700

    Pruksa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { SplashComponent } from './splash/splash.component';
+import { Admin1Component } from './admin1/admin1.component';
+import { Admin2Component } from './admin2/admin2.component';
+import { Admin3Component } from './admin3/admin3.component';
+import { Admin4Component } from './admin4/admin4.component';
+import { Admin5Component } from './admin5/admin5.component';
+import { Admin6Component } from './admin6/admin6.component';
+import { Admin7Component } from './admin7/admin7.component';
 
 // import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -20,7 +27,14 @@ import { SplashComponent } from './splash/splash.component';
     RegisterComponent,
     LoginComponent,
     MenuComponent,
-    SplashComponent
+    SplashComponent,
+    Admin1Component,
+    Admin2Component,
+    Admin3Component,
+    Admin4Component,
+    Admin5Component,
+    Admin6Component,
+    Admin7Component
   ],
   imports: [
     BrowserModule,
