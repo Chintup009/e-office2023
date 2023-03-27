f4942f41ea2e8bd545231db130d3b7b655802295
pttongnaka@gmail.com <lagacy.tm@gmail.com>
2023-03-27 12:16:45 +0700

    new merge

--- src/app/app.module copy.ts
+++ src/app/app.module copy.ts
@@ -8,7 +8,8 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { SplashComponent } from './splash/splash.component';
-
+<<<<<<< HEAD
+<<<<<<< HEAD
 import { Admin1Component } from './admin1/admin1.component';
 import { Admin2Component } from './admin2/admin2.component';
 import { Admin3Component } from './admin3/admin3.component';
@@ -16,12 +17,19 @@ import { Admin4Component } from './admin4/admin4.component';
 import { Admin5Component } from './admin5/admin5.component';
 import { Admin6Component } from './admin6/admin6.component';
 import { Admin7Component } from './admin7/admin7.component';
+=======
+import { Users1Component } from './users1/users1.component';
+import { Users2Component } from './users2/users2.component';
+>>>>>>> origin/Peerapol
+=======
 import { Users3Component } from './users3/users3.component';
 import { Users4Component } from './users4/users4.component';
 import { Users5Component } from './users5/users5.component';
-
+>>>>>>> origin/pitak
 
 // import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -31,16 +39,24 @@ import { Users5Component } from './users5/users5.component';
     LoginComponent,
     MenuComponent,
     SplashComponent,
+<<<<<<< HEAD
+<<<<<<< HEAD
     Admin1Component,
     Admin2Component,
     Admin3Component,
     Admin4Component,
     Admin5Component,
     Admin6Component,
-    Admin7Component,
+    Admin7Component
+=======
+    Users1Component,
+    Users2Component
+>>>>>>> origin/Peerapol
+=======
     Users3Component,
     Users4Component,
     Users5Component
+>>>>>>> origin/pitak
   ],
   imports: [
     BrowserModule,
