4ec64e4d905073f9cdfc2d1a525999e494f3dbc5
freeno12 <sirfreeno@gmail.com>
2023-03-27 11:51:08 +0700

    pitak

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RegisterComponent } from './register/register.component';
 import { SplashComponent } from './splash/splash.component';
+import { Users3Component } from './users3/users3.component';
+import { Users4Component } from './users4/users4.component';
+import { Users5Component } from './users5/users5.component';
 
 
 const routes: Routes = [
@@ -14,7 +17,10 @@ const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "menu", component: MenuComponent },
-  { path: "splash", component: SplashComponent },
+  { path: "splash", component: SplashComponent },  
+  { path: "users3", component: Users3Component},  
+  { path: "users4", component: Users4Component },  
+  { path: "users5", component: Users5Component },  
   { path: "", component: LoginComponent },
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { SplashComponent } from './splash/splash.component';
+import { Users3Component } from './users3/users3.component';
+import { Users4Component } from './users4/users4.component';
+import { Users5Component } from './users5/users5.component';
 
 // import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -20,7 +23,10 @@ import { SplashComponent } from './splash/splash.component';
     RegisterComponent,
     LoginComponent,
     MenuComponent,
-    SplashComponent
+    SplashComponent,
+    Users3Component,
+    Users4Component,
+    Users5Component
   ],
   imports: [
     BrowserModule,
