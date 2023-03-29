d3be2249c327c45ff12e679441c1f0f5d1bb3228
pttongnaka@gmail.com <lagacy.tm@gmail.com>
2023-03-29 15:37:04 +0700

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,4 +9,30 @@ import axios from 'axios';
 })
 export class LoginComponent {
 
+  public nameForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.nameForm = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  login() {
+    let data = {
+      "userEmail": this.nameForm.get('email')?.value,
+      "Password": this.nameForm.get('password')?.value
+    }
+    console.log(data);
+
+    axios.get("http://10.104.6.212:1337/api/rtarf-user-register-g1s?filters[userEmail][$eq]=" + this.nameForm.get('email')?.value).then(res => {
+      console.log(res.data);
+      if (res.data.data.length > 0) {
+        window.location.href = "/home"
+      } else {
+        alert("email หรือรหัสผ่านผิด")
+      }
+
+
+    })
+  }
 }
