72cd63e34c887f7359b09f1a76528e5f73e80849
Peerapol8684 <peerapolsripetch@gmail.com>
2023-03-30 14:28:19 +0700

    senddocument

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -27,6 +27,8 @@ export class LoginComponent {
     axios.get("http://10.104.6.212:1337/api/rtarf-user-register-g1s?filters[userEmail][$eq]=" + this.nameForm.get('email')?.value).then(res => {
       console.log(res.data);
       if (res.data.data.length > 0) {
+        let user = res.data.data[0].attributes
+        localStorage.setItem("user", JSON.stringify({ firstname: user.firstname, lastname: user.lastname, email: user.userEmail }))
         window.location.href = "/home"
       } else {
         alert("email หรือรหัสผ่านผิด")
